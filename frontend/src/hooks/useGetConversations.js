362560b65bc616b57213175cefe2ffe21f021399
BISWANATH BERA <berabiswanath338@gmail.com>
2024-03-09 11:26:37 +0530

    added new code base for realtime chat

---
+++ frontend/src/hooks/useGetConversations.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import { URL } from "../utils/apiUrl";
+
+const useGetConversations = () => {
+	const [loading, setLoading] = useState(false);
+	const [conversations, setConversations] = useState([]);
+
+	useEffect(() => {
+		const getConversations = async () => {
+			setLoading(true);
+			console.log(URL);
+			try {
+				const res = await fetch("/api/users");
+				const data = await res.json();
+				if (data.error) {
+					throw new Error(data.error);
+				}
+				setConversations(data);
+			} catch (error) {
+				toast.error(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		getConversations();
+	}, []);
+
+	return { loading, conversations };
+};
+export default useGetConversations;
